1bb55774826cff6d9c748257ee73de9d519303ba
zhycn <qzhservice@gmail.com>
2018-09-14 17:05:24 +0800

    Spring JPA Initial commit.

--- xbird-datasource-jpa/src/main/java/xbird/datasource/jpa/JpaApplication.java
+++ xbird-datasource-jpa/src/main/java/xbird/datasource/jpa/JpaApplication.java
@@ -2,8 +2,17 @@ package xbird.datasource.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement(mode = AdviceMode.ASPECTJ, proxyTargetClass = true)
+@EnableJpaRepositories({"xbird.datasource.jpa.repository"})
+@EntityScan("xbird.datasource.jpa.domain") // 实体类扫描
+@EnableJpaAuditing // 审计支持
 public class JpaApplication {
 
   public static void main(String[] args) throws Exception {
